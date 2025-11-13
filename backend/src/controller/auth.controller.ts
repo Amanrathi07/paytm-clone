24bea474089f8dd905ec63ef2d3bc6374d54e1f4
Amanrathi07 <amanrathi4321@gmail.com>
2025-11-13 17:35:26 +0530

    polish

--- backend/src/controller/auth.controller.ts
+++ backend/src/controller/auth.controller.ts
@@ -8,23 +8,25 @@ export async function signIn(req:Request , res: Response){
     try {
       
         const {name , email , password} =req.body ;
+        const existingUser = await userModel.findOne({email:email});
+        if (!name || !email || !password)
+    return res.status(400).json({ message: "All fields are required" });
+
+        if (existingUser) return res.status(404).json({message:"user alrady  exist "}) 
         const hassPassword = await bcrypt.hash(password,5);
-        const newUser = await new userModel({
+        const newUser = new userModel({
             name,
             email,
             password:hassPassword
         })
-        if (newUser){
-            jwt_generate(newUser._id,res);
-            await newUser.save();
+        await newUser.save();
+        jwt_generate(newUser._id,res);
+            
             return res.status(200).json({
-                message:"New User has been successfully added"
+                message:"User registered successfully"
             })
-        }else{
-            return res.status(404).json({
-                message:"can't add user "
-            })
-        }
+       
+        
     } catch (error) {
         console.log("error in fun signIN()",error) ;
     }
