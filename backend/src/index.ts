53e87bd65b17d6cccba7e6df9e7580e3e52da2e3
Amanrathi07 <amanrathi4321@gmail.com>
2025-11-17 07:39:17 +0530

    deployment try-1

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,4 +1,6 @@
 import express from "express";
+import path, { dirname } from "path";
+import { fileURLToPath } from "url";
 import dotenv  from "dotenv" ;
 import cors from "cors";
 import db_Connect from "./lib/db.js";
@@ -6,6 +8,10 @@ import cookieParser from "cookie-parser";
 
 import authRoute from "./routes/auth.routes.js";
 import accountRoute from "./routes/account.routes.js";
+
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 dotenv.config( );
 
@@ -32,10 +38,29 @@ app.get("/logout",(req ,res)=>{
   return res.status(200).json({ message: "Logged out" });
 })
 
+
+
+if (process.env.NODE_ENV === "production") {
+
+  const frontendPath = path.join(__dirname, "../../frontend/dist");
+
+  console.log("Serving static build from:", frontendPath);
+
+  app.use(express.static(frontendPath));
+
+  app.get(/.*/, (req, res) => {
+    res.sendFile(path.join(frontendPath, "index.html"));
+  });
+}
+
+
+
+
+
+
+
+
 const port:number = Number(process.env.SERVER_PORT)  ; 
-
-
-
 
 app.listen(port,()=>{
     db_Connect(process.env.MONGO_DB_URL!);
