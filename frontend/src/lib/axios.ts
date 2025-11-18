199aeee2ed1619e30bba5b2d21d7d8b1d7b6a9a0
Amanrathi07 <amanrathi4321@gmail.com>
2025-11-18 08:30:51 +0530

    next

--- frontend/src/lib/axios.ts
+++ frontend/src/lib/axios.ts
@@ -2,7 +2,14 @@ import axios from "axios";
 
 
 
+// export const axiosInstance  = axios.create({
+//     baseURL : import.meta.env.DEV ? "http://localhost:3000/v1" : "/v1",
+//     withCredentials:true
+// });
+
+
+
 export const axiosInstance  = axios.create({
-    baseURL : import.meta.env.DEV ? "http://localhost:3000/v1" : "/v1",
+    baseURL : import.meta.env.VITE_API_URL,
     withCredentials:true
 });
